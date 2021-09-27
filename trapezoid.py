48c21dc964b304572715a0faa793843567693874
Hampton <hambetn@dunwoody.edu>
2021-09-27 15:39:28 -0500

    added trapezoid test

--- trapezoid.py
+++ trapezoid.py
@@ -1,22 +1,24 @@
 import math
 
-def surfaceArea():
-    pass
-
 def area(baseA, baseB, hi):
-    area =  (baseA + baseB)/2 * hi
+    area =  round((baseA + baseB)/2 * hi, 2)
     return area
+
+def median(baseA, baseB):
+    median = round(baseA + baseB, 2)
+    return median
 
 def prompt():
     print()
     print("----------------------------------------------------------")
     print("PYTHON PROGRAM TO FIND AREA OF A TRAPEZOID")
     print("----------------------------------------------------------")
-    base1 = int(input("Please Enter the base1 : "))
-    base2 = int(input("Please Enter the base2 : "))
-    height = int(input("Please Enter the height : "))
+    base1 = int(input("Please Enter the base1: "))
+    base2 = int(input("Please Enter the base2: "))
+    height = int(input("Please Enter the height: "))
 
-    print("\nThe Area of a Trapezoid = ", area(base1, base2, height))
+    print("\nArea of a Trapezoid = ", area(base1, base2, height))
+    print("Median of a Trapezoid = ", median(base1, base2), "\n")
 
 if __name__ == '__main__':
     prompt()
---
+++ trapezoidTest.py
@@ -0,0 +1,21 @@
+import unittest
+import trapezoid
+
+class trapezoidTest(unittest.TestCase):
+
+    #passing tests
+
+    #Area Tests
+    def test_Area1(self):
+        assert(trapezoid.area(5, 10, 15) == 112.5)
+
+    #Median Tests    
+    def test_Median1(self):
+        assert(trapezoid.median(5, 10) == 15)
+        
+    #failing test
+    def test_volumeFail(self):
+       assert(trapezoid.area(5, 10, 15) == 0)
+
+if __name__ == '__main__':
+    unittest.main()
