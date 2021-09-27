79cecafd140e6028ae70d9b052c8f6ae50559a55
Hampton <hambetn@dunwoody.edu>
2021-09-27 15:15:36 -0500

    cube and cone test added

--- cube.py
+++ cube.py
@@ -1,21 +1,28 @@
 #change formula
 import math
 
-def surfaceArea():
-    pass
+def surfaceArea(side):
+    surfaceArea = round(6 * side * side, 2)
+    return surfaceArea
 
 def volume(side):
-    volume = side * side * side
+    volume = round(side * side * side, 2)
     return volume
+
+def latSurfaceArea(side):
+    latSurfaceArea = round(4 * side * side, 2)
+    return latSurfaceArea 
 
 def prompt():
     print()
     print("----------------------------------------------------------")
     print("PYTHON PROGRAM TO FIND VOLUME & SURFACE AREA OF A CUBE")
     print("----------------------------------------------------------")
-    sideLength = int(input("Please Enter the Length of any Side of a Cube:"))
-
-    print("\nThe Volume of a Cube = ", volume(sideLength))
+    sideLength = int(input("Please Enter the Length of any Side of a Cube: "))
+    
+    print("\nSurface Area of cube = ", surfaceArea(sideLength))
+    print("The Volume of a Cube = ", volume(sideLength))
+    print("Lateral Surface of Cube = ", latSurfaceArea(sideLength), "\n")
 
 if __name__ == '__main__':
     prompt()
--- cubeTest.py
+++ cubeTest.py
@@ -7,19 +7,19 @@ class coneTest(unittest.TestCase):
     
     #Surface Area Tests
     def test_surfaceArea1(self):
-        assert(cube.surfaceArea(5, 10) == 254.16)
+        assert(cube.surfaceArea(5) == 150)
 
     #Volume Tests    
     def test_volume1(self):
-        assert(cube.volume(5, 10) == 261.8)
+        assert(cube.volume(5) == 125)
 
     #Lateral Surface Area Tests
     def test_latSurfaceArea1(self):
-        assert(cube.latSurfaceArea(5, 10) == 175.62)
+        assert(cube.latSurfaceArea(5) == 100)
         
     #failing test
     def test_volumeFail(self):
-       assert(cube.volume(5, 10) == 0)
+       assert(cube.volume(5) == 0)
 
 if __name__ == '__main__':
     unittest.main()
