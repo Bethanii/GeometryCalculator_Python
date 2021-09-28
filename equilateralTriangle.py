d5da18da26775a41d5040216237c5e64e76663cd
Hampton <hambetn@dunwoody.edu>
2021-09-28 11:12:42 -0500

    added equilateral triangle tests

--- equilateralTriangle.py
+++ equilateralTriangle.py
@@ -1,20 +1,32 @@
 import math
 
-def surfaceArea():
-    pass
-
 def area(lth):
-    area =  (math.sqrt(3)/4) * lth * lth
+    area =  round((math.sqrt(3)/4) * lth * lth, 2)
     return area
+
+def perimeter(lth):
+    perimeter = round(3 * lth, 2)
+    return perimeter
+
+def semiPerimeter(lth):
+    semiPerimeter = round((3 * lth)/2, 2)
+    return semiPerimeter
+
+def altitude(lth):
+    altitude = round((1/2) * math.sqrt(3) * lth, 2)
+    return altitude
 
 def prompt():
     print()
     print("----------------------------------------------------------")
     print("PYTHON PROGRAM TO FIND AREA OF AN EQUILATERAL TRIANGLE")
     print("----------------------------------------------------------")
-    length = int(input("Please Enter Length of any side of an Equilateral Triangle : "))
+    length = int(input("Please Enter Length of any side of an Equilateral Triangle: "))
 
     print("\nArea of a Equilateral Triangle = ", area(length))
+    print("Perimeter of a Equilateral Triangle = ", perimeter(length))
+    print("Semi Perimeter of a Equilateral Triangle = ", semiPerimeter(length))
+    print("Altitude of a Equilateral Triangle = ", altitude(length),"\n")
 
 if __name__ == '__main__':
     prompt()
---
+++ equilateralTriangleTest.py
@@ -0,0 +1,29 @@
+import unittest
+import equilateralTriangle
+
+class equilateralTriangleTest(unittest.TestCase):
+
+    #passing tests
+
+    #Area Tests
+    def test_Area1(self):
+        assert(equilateralTriangle.area(5) == 10.83)
+
+    #Perimeter Tests    
+    def test_perimeter1(self):
+        assert(equilateralTriangle.perimeter(5) == 15)
+
+    #Semi Perimeter Tests
+    def test_semiPerimeter1(self):
+        assert(equilateralTriangle.semiPerimeter(5) == 7.5)
+
+    #Altitude Tests
+    def test_Altitude1(self):
+        assert(equilateralTriangle.altitude(5) == 4.33) 
+        
+    #failing test
+    def test_areaFail(self):
+       assert(equilateralTriangle.area(5) == 0)
+
+if __name__ == '__main__':
+    unittest.main()
