a0e581158272d9cb9bae3b1e85ad662811c66d15
Hampton <hambetn@dunwoody.edu>
2021-09-28 12:23:29 -0500

    added triangle tests

--- triangle.py
+++ triangle.py
@@ -1,21 +1,30 @@
 import math
 
-def surfaceArea():
-    pass
+def perimeter(sideA, sideB, sideC):
+    perimeter = round(sideA + sideB + sideC, 2)
+    return perimeter
 
-def area(rad, hi):
-    volume = math.pi * rad * rad * hi
-    return volume
+def semiPerimeter(sideA, sideB, sideC):
+    semiPerimeter = round((sideA + sideB + sideC)/2, 2)
+    return semiPerimeter
+
+def area(sideA, sideB, sideC):
+    sp = semiPerimeter(sideA, sideB, sideC)
+    area = round(math.sqrt(sp * (sp - sideA) * (sp - sideB) * (sp - sideC)), 2)
+    return area
 
 def prompt():
     print()
     print("----------------------------------------------------------")
     print("PYTHON PROGRAM TO FIND AREA OF A TRIANGLE")
     print("----------------------------------------------------------")
-    radius = int(input("Please Enter the radius :"))
-    height = int(input("Please Enter the height :"))
+    side1 = int(input("Please Enter the First side of a Triangle: "))
+    side2 = int(input("Please Enter the Second side of a Triangle: "))
+    side3 = int(input("Please Enter the Third side of a Triangle: "))
 
-    print("\nThe Area of a Triangle = ", area(radius, height))
+    print("\nThe Perimeter of a Triangle = ", perimeter(side1, side2, side3))
+    print("The Semi Perimeter of a Triangle = ", semiPerimeter(side1, side2, side3))
+    print("The Area of a Triangle = ", area(side1, side2, side3), "\n")
 
 if __name__ == '__main__':
     prompt()
---
+++ triangleTest.py
@@ -0,0 +1,25 @@
+import unittest
+import triangle
+
+class cuboidTest(unittest.TestCase):
+
+    #passing tests
+
+    #Perimeter Tests
+    def test_Perimeter1(self):
+        assert(triangle.perimeter(7, 5, 6) == 18)
+
+    #Semi Perimeter Tests    
+    def test_semiPerimeter1(self):
+        assert(triangle.semiPerimeter(7, 5, 6) == 9)
+
+    #Area Tests
+    def test_Area1(self):
+        assert(triangle.area(7, 5, 6) == 14.7)
+        
+    #failing test
+    def test_areaFail(self):
+       assert(triangle.area(7, 5, 6) == 0)
+
+if __name__ == '__main__':
+    unittest.main()
