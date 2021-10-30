87c485a2ae33c475b8a66f9cabff7d1bbe0247e0
Hampton <hambetn@dunwoody.edu>
2021-10-30 14:09:49 -0500

    added more shape html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
-import cylinder #<--why?
+import cylinder, cone, sphere#<--why?
 
 #flask plumbing
 app = Flask(__name__)
@@ -11,12 +11,17 @@ def mainForm():
    if request.method == "POST":
       sphere = request.form.get("sphere")
       cylinder = request.form.get("cylinder")
+      cone = request.form.get("cone")
       print("Selection was: ", sphere, cylinder) #prints to command line for trouble shooting
       if sphere == "on":
          print("User selected sphere") #prints to command line for trouble shooting
+         return redirect(url_for('sphereForm'))
       elif cylinder == "on":
          print("User selected cylinder") #prints to command line for trouble shooting
          return redirect(url_for('cylinderForm'))
+      elif cone == "on":
+         print("User selected cone") #prints to command line for trouble shooting
+         return redirect(url_for('coneForm'))
    return render_template("index.html")
 
 #flask route for the cylinder calculations page
@@ -30,6 +35,54 @@ def cylinderForm():
        vol = cylinder.volume(int(radius), int(height))
        return "User entered: Radius "+ str(radius) + " and Height: " + str(height) + ". <p>The Volume is: " + str(vol)
    return render_template("cylinder.html")
+
+#flask route for the cone calulation page
+@app.route("/cone", methods = ["GET", "POST"])
+def coneForm():
+   if request.method == "POST":
+       # getting input with name = fname in HTML form
+       radius = request.form.get("rad")
+       # getting input with name = lname in HTML form 
+       height = request.form.get("hgt") 
+
+       #Setting variables to cone functions 
+       vol = cone.volume(int(radius), int(height))
+       slant = cone.slant(int(radius), int(height))
+       surfaceArea = cone.surfaceArea(int(radius), int(height))
+       latSurfaceArea = cone.latSurfaceArea(int(radius), int(height))
+
+       #general output displaying user input
+       output = "User entered: Radius "+ str(radius) + " and Height: " + str(height)
+       #setting output to variables (for code structure)
+       volResult = ". <p>The Volume is: " +str(vol)
+       slantResult = ". <p>The Slant is: " + str(slant)
+       surfAreaResult = ". <p>The Surface Area is: " + str(surfaceArea)
+       latSurfAreaResult = ". <p>The Lateral Surface Area is: " + str(latSurfaceArea)
+
+       #returning variables as output
+       return output + volResult + slantResult + surfAreaResult + latSurfAreaResult
+   return render_template("cone.html")
+
+   #flask route for the sphere calculation page
+@app.route("/sphere", methods = ["GET", "POST"])
+def sphereForm():
+   if request.method == "POST":
+       # getting input with name = fname in HTML form
+       radius = request.form.get("rad")
+
+       #Setting variables to cone functions 
+       vol = sphere.volume(int(radius))
+       surfaceArea = sphere.surfaceArea(int(radius))
+
+       #general output displaying user input
+       output = "User entered: Radius "+ str(radius) 
+       #setting output to variables (for code structure)
+       volResult = ". <p>The Volume is: " +str(vol)
+       surfAreaResult = ". <p>The Surface Area is: " + str(surfaceArea)
+
+       #returning variables as output
+       return output + volResult +  surfAreaResult
+   return render_template("sphere.html")
 
 #more code here for the rest of the calculators: sphere, cube, etc.
   
--- cone.py
+++ cone.py
@@ -3,18 +3,22 @@ import math
 def selection():
     return 3
 
+#method to get cone surface area
 def surfaceArea(rad, hi):
     surfaceArea = round(math.pi * rad * (rad + math.sqrt((hi * hi) + (rad**2))), 2)
     return surfaceArea
 
+#method to get cone volume
 def volume(rad, hi):
     volume = round((math.pi * rad**2 * (hi/3)), 2)
     return volume
 
+#method to get cone lateral surface area
 def latSurfaceArea(rad, hi):
     latSurfaceArea = round(math.pi * rad * slant(rad, hi), 2)
     return latSurfaceArea
 
+#method to get cone slant
 def slant(rad, hi):
     slant = round(math.sqrt(rad**2 + hi * hi), 2)
     return slant
